527bf72df81b919f72800eae1e39fb5d76adeb49
Tracy Moscato <trcmoscato@gmail.com>
2013-04-04 19:36:45 -0400

    commit all things

--- local_settings.py
+++ local_settings.py
@@ -5,10 +5,10 @@ Includes keys for Twilio, etc.  Second stanza intended for Heroku deployment.
 '''
 
 # Uncommet to configure in file.
-ACCOUNT_SID = "AC802d88dd13f2e3f8d8bc3f9b95eb9f06" 
-AUTH_TOKEN = "a610e5b81d025523706a2bbc381bf5ac"
-BSSSPAM_APP_SID = "AP69a2d4c9cdceee480dfa9e9230c586c5"
-BSS_SPAM_ID = "+17032913341"
+ACCOUNT_SID = "ACc6bc4e62ba004615fe9bbcf9b244d2e8" 
+AUTH_TOKEN = "600da7b3d7304f152b0525d57b16fd28"
+BSSSPAM_APP_SID = "AP10ff593f6419c4ec108fe574f6e265e2"
+BSS_SPAM_ID = "+1 508-401-7549"
 
 
 # Begin Heroku configuration - configured through environment variables.
